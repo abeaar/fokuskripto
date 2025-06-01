88ab09ee5664d66cf168343ab1f6aa3455a40f80
faiz effendi <faizeffendi2004@gmail.com>
2025-06-01 15:37:10 +0700

    add: notification at android bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:fokuskripto/services/notification_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'pages/LoginPage.dart';
 import 'pages/RegisterPage.dart';
@@ -8,6 +10,14 @@ import 'pages/HomePage.dart';
 // Asumsikan kode CryptoListPage di atas ada di file yang sama atau diimpor
 void main() async {
   await Hive.initFlutter();
+  await NotificationService().init();
+
+  await Permission.notification.isDenied.then((value) {
+    if (value) {
+      Permission.notification.request();
+    }
+  });
+
   runApp(const MyApp());
 }
 
