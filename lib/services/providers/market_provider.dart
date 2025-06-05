181a63ad063af9280f6fb7d72a700ac5a9353bc5
abeaar <abraarjh@gmail.com>
2025-06-05 23:11:19 +0700

    update wth

--- lib/services/providers/market_provider.dart
+++ lib/services/providers/market_provider.dart
@@ -14,8 +14,8 @@ class MarketProvider extends ChangeNotifier {
   DateTime? _lastUpdated;
   String _sortField = 'market_cap_rank';
   bool _isAscending = true;
-  static const int _refreshIntervalSeconds = 10;
-  static const int _forceRefreshIntervalMinutes = 60;
+  static const int _refreshIntervalSeconds = 300;
+  static const int _forceRefreshIntervalMinutes = 10;
 
   // Getters
   bool get isLoading => _isLoading;
